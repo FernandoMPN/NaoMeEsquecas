f39ca1021954e392ecaa5700a10a59da34434891
LeiseR <willy.leiser@gmail.com>
2020-05-18 20:45:07 -0300

    Primeiros ajustes add : Home, agendamentos e colors patters

---
+++ src/Pages/Agendamento/Agendamento.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+function Agendamento(){
+
+    return(
+        <Text>Deu certo</Text>
+    )
+}
+export default Agendamento
---
+++ src/Utils/colors.js
@@ -0,0 +1,21 @@
+export const purpleColors={
+    darkerPurple:'#532375',
+    darkPurple:'#6B398E',
+    middlePurple:'#8654A8',
+    purple:'#A274C2',
+    litePurple:'#C9AFDB',
+    literPurple:'#EBDCF5',
+    evenLiterPurple:'#FAF2FF'
+}
+
+export const blackAndWhite={
+    blackBlack:'#202833',
+    black:'#404B5A',
+    darkerGray:'#6D7989',
+    darkGray:'#929FB1',
+    middleGray:'#8B9FBA',
+    gray:'#CCD2DB',
+    liteGray:'#D5DCE5',
+    literGray:'#E0E5EB',
+    evenLiterGray:'#F8F9FA'
+}
