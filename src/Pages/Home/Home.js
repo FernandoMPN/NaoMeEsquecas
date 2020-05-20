6cad3982ffdc0c3cbdbab21ac7c7646c790946db
RenanRicoldi <renan.ricoldi.frois@gmail.com>
2020-05-20 12:25:58 -0300

    Base da Home feita

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import {Text} from 'react-native'
+import { Text, View, SafeAreaView } from 'react-native'
 
-function Home(){
+import Styles from './Styles'
 
-    return(
-        <Text>Deu certo</Text>
-    )
+function Home() {
+  return(
+    <View style={ Styles.mainView }>
+        <View style={ Styles.topBar }/>
+        <View style={ Styles.image }/>
+        <View style={ Styles.button }>
+            <Text style={ Styles.buttonText }>Quero Ser Atendido</Text>
+        </View>
+        <View style={ Styles.button }>
+            <Text style={ Styles.buttonText }>Quero Atender</Text>
+        </View>
+        <Text style={ Styles.aboutText }>Sobre o app</Text>
+        <View style={ Styles.shapes }/>
+    </View>
+  ) 
 }
 export default Home
---
+++ src/Pages/Home/Styles.js
@@ -0,0 +1,47 @@
+import { StyleSheet, Dimensions } from 'react-native'
+
+const width= Dimensions.get('window').width
+const height=Dimensions.get('window').height
+
+const Styles = StyleSheet.create({
+    mainView:{ flex: 1,
+        backgroundColor: '#fff', 
+        alignItems: "center", 
+        justifyContent: 'space-between'
+    },
+    topBar:{
+        width,
+        height:width/18,
+        backgroundColor:'#6B398E'
+    },
+    image:{
+        width: 282,
+        height: 119,
+        backgroundColor:'#b9b9b9'
+    },
+    button: {
+        width: '85%', 
+        height: '14%',
+        borderRadius: 8,
+        backgroundColor: '#A371C3',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color:'#fff',
+        fontWeight:'bold',
+        fontSize:26,
+        textAlign: 'center'
+    },
+    shapes: {
+        width,
+        height: 33,
+        backgroundColor:'#6B398E'
+    },
+    aboutText: {
+        color:'#6B398E',
+        fontWeight:'bold',
+        fontSize:18
+    },
+})
+
+export default Styles
