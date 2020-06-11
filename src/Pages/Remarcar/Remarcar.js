fefaadabe295ff242c635478e27aea7bb27eb7fd
RenanRicoldi <renan.ricoldi.frois@gmail.com>
2020-06-11 20:26:36 -0300

    Remarcar pronto

--- src/Pages/Remarcar/Remarcar.js
+++ src/Pages/Remarcar/Remarcar.js
@@ -2,99 +2,38 @@ import React, { useState } from 'react'
 import {View,
         Text,
         SafeAreaView,
-        StatusBar,
         TouchableOpacity,
         Image,
         Modal,
-        ScrollView,} from 'react-native'
-import {Picker} from '@react-native-community/picker'
+        ScrollView} from 'react-native'
 
-import Styles, {TextStyle, ModalStyle} from './Styles'
-import colors from '../../Utils/colors'
+import Styles, {TextStyle} from './Styles'
+import {getTextDate} from '../RegisterPaciente/RegisterPaciente'
 
-const dayOfWeek = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo']
-const hours = [8,9,10,11,12,13,14,15,16,17,18,19]
-
-const getTextDate = (day, time) => {
-
-    const endTime = time + 1
-
-    return dayOfWeek[day] + ', ' + time + '-' + endTime + 'h'
-}
-
-function AddNewDateModal({ addNewCallback, exit }){
-
-    const [currentDay, setDay] = useState('none')
-    const [currentHour, setHour] = useState('none')
-    
-    const isButtonDisabled = currentDay === 'none' || currentHour === 'none'
-
-    const returnSelectedDate = () => {
-        addNewCallback({
-            id: Date.now(),
-            day: currentDay,
-            time: currentHour
-        })
-    }
-
-    return(
-        <TouchableOpacity activeOpacity={1} onPress={exit} style={ ModalStyle.Container }>
-            <TouchableOpacity activeOpacity={1} style={ ModalStyle.MainArea }>
-                <Text>
-                    <Text style={ TextStyle.modalHeader }>Selecione</Text>
-                    <Text style={ TextStyle.modalHeaderBold }> um dia da semana e um horário </Text>
-                    <Text style={ TextStyle.modalHeader }>que você tem disponível</Text>
-                </Text>
-
-                <Text style={ TextStyle.textMargin }>As consultas tem duração de 1 hora</Text>
-    
-                <View style={ ModalStyle.pickerArea }>
-
-                    <Picker
-                        selectedValue={currentDay}
-                        onValueChange={(itemValue, itemIndex) => setDay(itemValue)}
-                    >
-                        <Picker.Item label='Selecione o dia' value='none' />
-
-                        {dayOfWeek.map((item, index) =>  <Picker.Item key={item} label={item} value={index} />)}
-                    
-                    </Picker>
-
-                    <Picker
-                        selectedValue={currentHour}
-                        onValueChange={(itemValue, itemIndex) => setHour(itemValue)}
-                    >
-                        <Picker.Item label='Selecione o horário' value='none' />
-
-                        {hours.map((item, index) =>  <Picker.Item key={item+'h'} label={item + 'h'} value={item} />)}
-                    
-                    </Picker>
-
-                </View>
-
-                <TouchableOpacity onPress={returnSelectedDate} disabled={isButtonDisabled} style={ [Styles.button, isButtonDisabled? {backgroundColor:'gray'}:null] }>
-                    <Text style={ TextStyle.buttonTextSemiBold }>Adicionar</Text>
-                </TouchableOpacity>
-
-            </TouchableOpacity>
-        </TouchableOpacity>
-    )
-
-}
+import AddNewDateModal from '../../Components/AddNewDateModal/AddNewDateModal'
 
 function Remarcar({ navigation }){
 
-    /*
-    * TODO: userHours deve receber o estado anterior
-    */
     const [userHours, setNewHour] = useState([])
     const [isModalVisible, setModalVisibility] = useState(false)
+    const [addMoreDisabled, disableAddMore] = useState(false)
 
     const removeIcon = require('../../Assets/Icons/remove/remove.png')
+    const addNewIcon = require('../../Assets/Icons/addnew/addnew.png')
     const isButtonDisabled = userHours.length === 0
 
     const changeModalVisibility = () => {
         setModalVisibility(!isModalVisible)
+    }
+
+    const checkAddMore = (itemsAdded) => {
+        
+        if(itemsAdded === 6){
+            disableAddMore(true)
+        }else{
+            disableAddMore(false)
+        }
+
     }
 
     const addNewItem = (item) => {
@@ -117,16 +56,23 @@ function Remarcar({ navigation }){
         if(isTherePrev)
             return
 
-        setNewHour(prev => prev.concat(item))
+        setNewHour(prev => {
+            checkAddMore(prev.length + 1)
+            return prev.concat(item)
+        })
+        
 
     }
 
     const removeItem = (itemID) => {
-        setNewHour(prev => prev.filter(item => item.id !== itemID))
+        setNewHour(prev => {
+            checkAddMore(prev.length - 1)
+            return prev.filter(item => item.id !== itemID)
+        })
     }
 
     const HourItem = ({date}) => {
-
+ 
         const text = getTextDate(date.day, date.time)
 
         return(
@@ -140,41 +86,39 @@ function Remarcar({ navigation }){
 
     }
 
-    /*
-    * TODO: userHours deve enviar os novos horários
-    */
     const setHours = () => {
         console.log(userHours)
         navigation.navigate('MainPage')
     }
 
     return(
-        <SafeAreaView style={{ backgroundColor: colors.statusBar }}>
+        <SafeAreaView>
             <View style={ Styles.MainContainer }>
 
-                <Modal visible={isModalVisible} transparent={true} animationType='fade'>
+                <Modal visible={isModalVisible} transparent={true} animationType="fade">
                     <AddNewDateModal exit={changeModalVisibility} addNewCallback={addNewItem}/>
                 </Modal>
 
                 <Text style={ TextStyle.header }>Deseja mesmo Remarcar? Nos diga então quais horários você prefere ser atendido</Text>
 
-                <TouchableOpacity activeOpacity={0.8} onPress={changeModalVisibility}>
-                    <View style={ Styles.addNewButton }>
+                <TouchableOpacity activeOpacity={0.8} disabled={addMoreDisabled} onPress={changeModalVisibility}>
+                    <View style={ [Styles.addNewButton, addMoreDisabled? {backgroundColor: "gray"}:null]}>
                         <Text style={ TextStyle.buttonText }>Adicionar novo horário</Text>
 
-                        <View style={ Styles.addNewIcon }/>
-
+                        <View style={ Styles.addNewIcon }>
+                            <Image source={addNewIcon}/>
+                        </View>
                     </View>
                 </TouchableOpacity>
 
                 <View style={ Styles.itensContainer }>
-                    <ScrollView style={{width: '100%'}}>
-                        {userHours.reverse().map(date => <HourItem key={date.day + '#' + date.time} date={date}/>)}
+                    <ScrollView style={{width: "100%"}}>
+                        {userHours.map(date => <HourItem key={date.day + "#" + date.time} date={date}/>)}
                     </ScrollView>
                 </View>
 
-                <TouchableOpacity onPress={setHours} disabled={isButtonDisabled} style={ [Styles.button, isButtonDisabled? {backgroundColor:'gray'}:null] }>
-                    <Text style={ TextStyle.buttonTextSemiBold }>Pronto</Text>
+                <TouchableOpacity onPress={setHours} disabled={isButtonDisabled} style={ [Styles.button, isButtonDisabled? {backgroundColor:"gray"}:null] }>
+                    <Text style={ TextStyle.buttonTextSemiBold }>Próximo</Text>
                 </TouchableOpacity>
 
             </View>
